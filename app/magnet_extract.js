6f66a5dc0aea8cc9240f58fe470f8fdc3652d96f
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2022-06-07 11:01:53 -0300

    Core Logic
    Using request, cheerio, valid-url to make magic web scrapping

---
+++ app/magnet_extract.js
@@ -0,0 +1,101 @@
+const request = require('request')
+const cheerio = require('cheerio')
+const urlParser = require('url')
+const validUrl = require('valid-url')
+
+let options = {
+    url: '',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0'
+    },
+    timeout: 6000
+}
+
+function extractMagnet(urls) {
+    console.log('Extracting magnets links...')
+
+    function getMagnetLinks(url, resolve) {
+        options.url = url
+
+        request(options, (err, resp, body) => {
+            let links = []
+
+            if (err) {
+                resolve([])
+                return;
+            }
+
+            const $ = cheerio.load(body)
+
+            $('a[href^="magnet:"]').each((i, elem) => {
+                let link = elem.attribs['href']
+
+                if (isValidMagnetLink(link)) {
+                    links.push(link)
+                }
+            })
+
+            resolve(links)
+        })
+    }
+
+    function validUrls(item) {
+        return validUrl.isUri(item)
+    }
+
+
+    function mountPromises(urls) {
+        return urls.filter(validUrls).map(url => {
+            return new Promise((resolve) => {
+                getMagnetLinks(url, resolve)
+            })
+        })
+    }
+
+    return new Promise((resolve, reject) => {
+        Promise.all(mountPromises(urls))
+            .then((data) => {
+                const urls = data.length ?
+                    getMagnetDto(data.reduce((acc, curr) => [...acc, ...curr], []))
+                    : []
+
+                resolve({ urls })
+            })
+            .catch(reject)
+    })
+}
+
+function getMagnetDto(googleMagnetRes) {
+    return googleMagnetRes.map((link) => {
+        return {
+            uri: link,
+            name: extractTorrentNameFromLink(link)
+        }
+    })
+}
+
+function extractTorrentNameFromLink(link) {
+    let name = new urlParser.URL(link).searchParams.get('dn')
+
+    if (!name) {
+        let dnF = link.substring(link.indexOf('dn='))
+
+        name = decodeURI(dnF.substring(3, dnF.indexOf('&amp')))
+    }
+
+    return name
+}
+
+function isValidMagnetLink(link) {
+    return link.startsWith('magnet:')
+}
+
+module.exports = {
+    extractMagnet: (urls) => {
+        return new Promise((resolve, reject) => {
+            extractMagnet(urls)
+                .then(resolve)
+                .catch(reject)
+        })
+    }
+}
---
+++ app/seed_extract.js
@@ -0,0 +1,50 @@
+const request = require('request')
+const cheerio = require('cheerio')
+const validUrl = require('valid-url')
+
+let options = {
+    url: '',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0'
+    },
+    timeout: 10000
+}
+
+const googleResultSelector = '#search a'
+
+function getSeeds(query) {
+    options.url = `https://www.google.com.br/search?q=${query.replace(/ /g, '+')}+download+torrent`
+    return new Promise((resolve, reject) => {
+        console.log('Retrieving sources from source')
+
+        request(options, (err, resp, body) => {
+            if (err) {
+                reject('Error fetching data sources. Try again.')
+                return
+            }
+
+            let urls = []
+            const $ = cheerio.load(body)
+            
+            $(googleResultSelector).each((i, result) => {
+                urls.push(result.attribs['href'])
+            })
+
+            resolve(urls)
+        })
+    })
+}
+
+module.exports = {
+    extractSeeds: (data) => {
+        return new Promise((resolve, reject) => {
+            if (validUrl.isUri(data)) {
+                resolve([data])
+            } else {
+                getSeeds(data)
+                    .then(resolve)
+                    .catch(reject)        
+            }
+        })
+    }
+}
