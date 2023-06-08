32e4129d18fb589a00ffa53440e7a99537561f5a
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2023-06-07 22:33:45 -0300

    color update 🏳️‍🌈
    huuur.. agora o código apresenta uma coloração fantástica, com cores escolhidas a Dedo pelos maiores designers da região ☝️🤓 trazendo mais conforto e...🏳️‍🌈... urrr... usabilidade 🤓☝️

--- index.js
+++ index.js
@@ -5,21 +5,29 @@ console.log('\x1b[32mDigite uma pesquisa:\x1b[0m ');
 process.stdin.on('data', (data) => {
     const search = data.trim();
 
-controller.extractTorrents(search).then(data => {
+    controller.extractTorrents(search).then(data => {
         if (!data.urls.length) {
-            console.log('No data found');
+            console.log('\x1b[31mNo data found\x1b[0m');
             return;
         }
 
         console.log('Results:\n')
         var i = 0;
+        const colors = ['\x1b[34m', '\x1b[33m', '\x1b[32m', '\x1b[31m', '\x1b[35m', '\x1b[33m', '\x1b[38;5;208m', '\x1b[38;5;39m', '\x1b[90m', '\x1b[38;5;205m', '\x1b[36m', '\x1b[38;5;124m']; // Array de códigos de cores ANSI
+
         data.urls.forEach(item => {
             i++;
-            console.log(item.name, item.uri)
+            const randomIndex = Math.floor(Math.random() * colors.length);
+            const color = colors[randomIndex];
+            const colorCode = `\x1b[${color}m`;
+            const resetCode = '\x1b[0m';
+
+            console.log(`${colorCode}${item.name}${resetCode}`, `${colorCode}${item.uri}${resetCode}`);
             console.log('------------');
         });
-        console.log("Total = ", i)
-    }).catch(err => console.log(err))
+
+        console.log("Total = ", i);
+    }).catch(err => console.log(err));
 
     process.stdin.pause();
 });
