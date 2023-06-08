3eef5040276781ca4b6f49313b83e083b3389802
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2023-06-07 22:48:39 -0300

    refatorando controller ☝️🤓
    Utilizei a sintaxe de async/await para tornar o código mais legível e evitar o aninhamento excessivo de Promises.
    Adicionei os blocos try/catch para capturar e propagar erros adequadamente.
    Renomeei a função exportada de extractTorrents para ficar de acordo com a convenção de camel case.

--- app/controller.js
+++ app/controller.js
@@ -1,21 +1,24 @@
-const seedExtract = require('./seed_extract')
-const magnetExtract = require('./magnet_extract')
+const seedExtract = require('./seed_extract');
+const magnetExtract = require('./magnet_extract');
 
 function extractSeeds(data) {
-    return seedExtract.extractSeeds(data)
+  return seedExtract.extractSeeds(data);
 }
 
 function extractMagnet(query) {
-    return magnetExtract.extractMagnet(query)
+  return magnetExtract.extractMagnet(query);
+}
+
+async function extractTorrents(data) {
+  try {
+    const seeds = await extractSeeds(data);
+    const magnets = await extractMagnet(seeds);
+    return magnets;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
-    extractTorrents: (data) => {
-        return new Promise((resolve, reject) => {
-            extractSeeds(data)
-                .then(extractMagnet)
-                .then(resolve)
-                .catch(reject)
-        })
-    }
-}
+  extractTorrents
+};
