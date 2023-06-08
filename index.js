c44b376f2adec3eb9a44f9874fa61966710220aa
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2023-06-07 22:42:51 -0300

    refatoração fudida 💩
    NEsse novo e incrível update eu refatorei o código, resumindo, mudei tudo pra mudar porra nenhuma( como uma boa refatoração deve ser, né gente 🤓). Nessa versão, utilizei o módulo readline do Node.js para lidar com a entrada e saída do console de forma mais elegante. Além disso, removi o uso do process.stdin.resume() e utilizei uma função de callback no método rl.question para obter a entrada do usuário. Também fiz algumas pequenas alterações para seguir as convenções de estilo, como a formatação das linhas e espaçamento adequado entre os parâmetros. pqp🤓☝️

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
-const controller = require('./app/controller')
-process.stdin.setEncoding('utf8');
+const controller = require('./app/controller');
+const readline = require('readline');
+
 console.log('WELCOME!');
+
 function performSearch() {
-    console.log('[Enter] to exit');
-    console.log('\x1b[32mWhat do you want to search?\x1b[0m ');
-    process.stdin.once('data', (data) => {
-        const search = data.trim();
-        if (search === '') {
-            process.exit(); // Encerrar o programa se a entrada estiver vazia
-          }
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-        controller.extractTorrents(search).then(data => {
-            if (!data.urls.length) {
-                console.log('\x1b[31mNo data found...\x1b[0m');
-                performSearch(); // Chamando a função novamente para permitir nova pesquisa
-                return;
-            }
+  console.log('[Enter] to exit');
+  rl.question('\x1b[32mWhat do you want to search?\x1b[0m ', (search) => {
+    rl.close();
 
-            console.log('\x1b[32mResults:\x1b[0m\n');
-            var i = 0;
-            const colors = ['\x1b[34m', '\x1b[33m', '\x1b[32m', '\x1b[31m', '\x1b[35m', '\x1b[33m', '\x1b[38;5;208m', '\x1b[38;5;39m', '\x1b[90m', '\x1b[38;5;205m', '\x1b[36m', '\x1b[38;5;124m'];
-            data.urls.forEach(item => {
-                i++;
-                const randomIndex = Math.floor(Math.random() * colors.length);
-                const color = colors[randomIndex];
-                const colorCode = `\x1b[${color}m`;
-                const resetCode = '\x1b[0m';
+    if (search === '') {
+      process.exit(); // Encerrar o programa se a entrada estiver vazia
+    }
 
-                console.log(`${colorCode}${item.name}${resetCode}`, `${colorCode}${item.uri}${resetCode}`);
-                console.log('------------');
-            });
+    controller.extractTorrents(search)
+      .then(data => {
+        if (!data.urls.length) {
+          console.log('\x1b[31mNo data found...\x1b[0m');
+          performSearch(); // Chamando a função novamente para permitir nova pesquisa
+          return;
+        }
 
-            console.log("Total = ", i);
+        console.log('\x1b[32mResults:\x1b[0m\n');
+        let i = 0;
+        const colors = ['\x1b[34m', '\x1b[33m', '\x1b[32m', '\x1b[31m', '\x1b[35m', '\x1b[33m', '\x1b[38;5;208m', '\x1b[38;5;39m', '\x1b[90m', '\x1b[38;5;205m', '\x1b[36m', '\x1b[38;5;124m'];
+        data.urls.forEach(item => {
+          i++;
+          const randomIndex = Math.floor(Math.random() * colors.length);
+          const color = colors[randomIndex];
+          const colorCode = `\x1b[${color}m`;
+          const resetCode = '\x1b[0m';
 
-            performSearch(); // Chamando a função novamente para permitir nova pesquisa
-        }).catch(err => console.log(err));
-    });
+          console.log(`${colorCode}${item.name}${resetCode}`, `${colorCode}${item.uri}${resetCode}`);
+          console.log('------------');
+        });
 
-    process.stdin.resume();
+        console.log("Total =", i);
+        performSearch(); // Chamando a função novamente para permitir nova pesquisa
+      })
+      .catch(err => console.log(err));
+  });
 }
 
 performSearch(); // Chamando a função pela primeira vez para iniciar o processo de pesquisa
