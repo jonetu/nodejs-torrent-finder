75858d084002cbcee430a0f95b8570983861d6dd
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2023-06-07 22:53:35 -0300

    refatorando seed
    ☝️🤓ultima refatoração. Não vou mexer mais nessa bosta.. eu acho

--- app/seed_extract.js
+++ app/seed_extract.js
@@ -1,50 +1,52 @@
-const request = require('request')
-const cheerio = require('cheerio')
-const validUrl = require('valid-url')
+const request = require('request');
+const cheerio = require('cheerio');
+const validUrl = require('valid-url');
 
-let options = {
-    url: '',
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0'
-    },
-    timeout: 10000
-}
+const options = {
+  url: '',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0'
+  },
+  timeout: 10000
+};
 
-const googleResultSelector = '#search a'
+const googleResultSelector = '#search a';
 
 function getSeeds(query) {
-    options.url = `https://www.google.com.br/search?q=${query.replace(/ /g, '+')}+download+torrent`
-    return new Promise((resolve, reject) => {
-        console.log('Retrieving sources from source')
+  const formattedQuery = query.replace(/ /g, '+');
+  options.url = `https://www.google.com.br/search?q=${formattedQuery}+download+torrent`;
 
-        request(options, (err, resp, body) => {
-            if (err) {
-                reject('Error fetching data sources. Try again.')
-                return
-            }
+  return new Promise((resolve, reject) => {
+    console.log('Retrieving sources from source');
 
-            let urls = []
-            const $ = cheerio.load(body)
-            
-            $(googleResultSelector).each((i, result) => {
-                urls.push(result.attribs['href'])
-            })
+    request(options, (err, resp, body) => {
+      if (err) {
+        reject('Error fetching data sources. Try again.');
+        return;
+      }
 
-            resolve(urls)
-        })
-    })
+      const urls = [];
+      const $ = cheerio.load(body);
+
+      $(googleResultSelector).each((i, result) => {
+        urls.push(result.attribs.href);
+      });
+
+      resolve(urls);
+    });
+  });
 }
 
 module.exports = {
-    extractSeeds: (data) => {
-        return new Promise((resolve, reject) => {
-            if (validUrl.isUri(data)) {
-                resolve([data])
-            } else {
-                getSeeds(data)
-                    .then(resolve)
-                    .catch(reject)        
-            }
-        })
-    }
-}
+  extractSeeds: (data) => {
+    return new Promise((resolve, reject) => {
+      if (validUrl.isUri(data)) {
+        resolve([data]);
+      } else {
+        getSeeds(data)
+          .then(resolve)
+          .catch(reject);
+      }
+    });
+  }
+};
