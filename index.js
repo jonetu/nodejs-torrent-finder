92b06d15341cf2569c8b5153d1889be89d7828fd
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2023-06-07 22:39:31 -0300

    atualização im.. por.. tan..tíssima🤓
    nesse novo UPDATE de NERD FUDIDO agora o programa não finaliza de uma vez, ele repete e assim não precisa executar várias vezes ☝️🤓 trazendo mais conforto e... urrr... usabilidade 🤓☝️

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 const controller = require('./app/controller')
 process.stdin.setEncoding('utf8');
+console.log('WELCOME!');
+function performSearch() {
+    console.log('[Enter] to exit');
+    console.log('\x1b[32mWhat do you want to search?\x1b[0m ');
+    process.stdin.once('data', (data) => {
+        const search = data.trim();
+        if (search === '') {
+            process.exit(); // Encerrar o programa se a entrada estiver vazia
+          }
 
-console.log('\x1b[32mDigite uma pesquisa:\x1b[0m ');
-process.stdin.on('data', (data) => {
-    const search = data.trim();
+        controller.extractTorrents(search).then(data => {
+            if (!data.urls.length) {
+                console.log('\x1b[31mNo data found...\x1b[0m');
+                performSearch(); // Chamando a função novamente para permitir nova pesquisa
+                return;
+            }
 
-    controller.extractTorrents(search).then(data => {
-        if (!data.urls.length) {
-            console.log('\x1b[31mNo data found\x1b[0m');
-            return;
-        }
+            console.log('\x1b[32mResults:\x1b[0m\n');
+            var i = 0;
+            const colors = ['\x1b[34m', '\x1b[33m', '\x1b[32m', '\x1b[31m', '\x1b[35m', '\x1b[33m', '\x1b[38;5;208m', '\x1b[38;5;39m', '\x1b[90m', '\x1b[38;5;205m', '\x1b[36m', '\x1b[38;5;124m'];
+            data.urls.forEach(item => {
+                i++;
+                const randomIndex = Math.floor(Math.random() * colors.length);
+                const color = colors[randomIndex];
+                const colorCode = `\x1b[${color}m`;
+                const resetCode = '\x1b[0m';
 
-        console.log('Results:\n')
-        var i = 0;
-        const colors = ['\x1b[34m', '\x1b[33m', '\x1b[32m', '\x1b[31m', '\x1b[35m', '\x1b[33m', '\x1b[38;5;208m', '\x1b[38;5;39m', '\x1b[90m', '\x1b[38;5;205m', '\x1b[36m', '\x1b[38;5;124m']; // Array de códigos de cores ANSI
+                console.log(`${colorCode}${item.name}${resetCode}`, `${colorCode}${item.uri}${resetCode}`);
+                console.log('------------');
+            });
 
-        data.urls.forEach(item => {
-            i++;
-            const randomIndex = Math.floor(Math.random() * colors.length);
-            const color = colors[randomIndex];
-            const colorCode = `\x1b[${color}m`;
-            const resetCode = '\x1b[0m';
+            console.log("Total = ", i);
 
-            console.log(`${colorCode}${item.name}${resetCode}`, `${colorCode}${item.uri}${resetCode}`);
-            console.log('------------');
-        });
+            performSearch(); // Chamando a função novamente para permitir nova pesquisa
+        }).catch(err => console.log(err));
+    });
 
-        console.log("Total = ", i);
-    }).catch(err => console.log(err));
+    process.stdin.resume();
+}
 
-    process.stdin.pause();
-});
+performSearch(); // Chamando a função pela primeira vez para iniciar o processo de pesquisa
