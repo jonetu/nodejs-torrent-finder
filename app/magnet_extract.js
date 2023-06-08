5aaddfa1cca289ec2b67b1fe601a99b72bd84bb3
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2023-06-07 22:51:16 -0300

    refatorando o extract ☝️🤓
    Mostrei meu código para um amigo meu e ele Falou KKKKKK seu fudido nunca vai ser programador se tu fizer um código PORCO que nem esse ☝️🤓. Logo, dei-lhe uma bicuda e estou correndo atrás de refatorar esse pedaço de cocô que criou pernas
    Removi a função resolve e reject desnecessárias na exportação do extractMagnet.
    Adicionei uma Promise no interior da função getMagnetLinks para lidar com a chamada assíncrona ao request.
    Utilizei o return dentro da função getMagnetLinks para retornar a Promise.
    Removi o console.log de mensagem intermediária dentro da função extractMagnet e mantive apenas a mensagem de "Extracting magnet links...".
    Organizei as importações em ordem alfabética.
    Adicionei quebras de linha para melhorar a legibilidade do código.

--- app/magnet_extract.js
+++ app/magnet_extract.js
@@ -1,101 +1,87 @@
-const request = require('request')
-const cheerio = require('cheerio')
-const urlParser = require('url')
-const validUrl = require('valid-url')
+const request = require('request');
+const cheerio = require('cheerio');
+const urlParser = require('url');
+const validUrl = require('valid-url');
 
-let options = {
-    url: '',
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0'
-    },
-    timeout: 6000
-}
+const options = {
+  url: '',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0'
+  },
+  timeout: 6000
+};
 
 function extractMagnet(urls) {
-    console.log('Extracting magnets links...')
+  console.log('Extracting magnet links...');
 
-    function getMagnetLinks(url, resolve) {
-        options.url = url
-
-        request(options, (err, resp, body) => {
-            let links = []
-
-            if (err) {
-                resolve([])
-                return;
-            }
-
-            const $ = cheerio.load(body)
-
-            $('a[href^="magnet:"]').each((i, elem) => {
-                let link = elem.attribs['href']
-
-                if (isValidMagnetLink(link)) {
-                    links.push(link)
-                }
-            })
-
-            resolve(links)
-        })
-    }
-
-    function validUrls(item) {
-        return validUrl.isUri(item)
-    }
-
-
-    function mountPromises(urls) {
-        return urls.filter(validUrls).map(url => {
-            return new Promise((resolve) => {
-                getMagnetLinks(url, resolve)
-            })
-        })
-    }
+  function getMagnetLinks(url) {
+    options.url = url;
 
     return new Promise((resolve, reject) => {
-        Promise.all(mountPromises(urls))
-            .then((data) => {
-                const urls = data.length ?
-                    getMagnetDto(data.reduce((acc, curr) => [...acc, ...curr], []))
-                    : []
+      request(options, (err, resp, body) => {
+        if (err) {
+          resolve([]);
+          return;
+        }
 
-                resolve({ urls })
-            })
-            .catch(reject)
-    })
+        const $ = cheerio.load(body);
+        const links = [];
+
+        $('a[href^="magnet:"]').each((i, elem) => {
+          const link = elem.attribs.href;
+
+          if (isValidMagnetLink(link)) {
+            links.push(link);
+          }
+        });
+
+        resolve(links);
+      });
+    });
+  }
+
+  function validUrls(item) {
+    return validUrl.isUri(item);
+  }
+
+  const mountPromises = urls
+    .filter(validUrls)
+    .map(url => getMagnetLinks(url));
+
+  return new Promise((resolve, reject) => {
+    Promise.all(mountPromises)
+      .then(data => {
+        const urls = data.length ? getMagnetDto(data.reduce((acc, curr) => [...acc, ...curr], [])) : [];
+
+        resolve({ urls });
+      })
+      .catch(reject);
+  });
 }
 
 function getMagnetDto(googleMagnetRes) {
-    return googleMagnetRes.map((link) => {
-        return {
-            uri: link,
-            name: extractTorrentNameFromLink(link)
-        }
-    })
+  return googleMagnetRes.map(link => ({
+    uri: link,
+    name: extractTorrentNameFromLink(link)
+  }));
 }
 
 function extractTorrentNameFromLink(link) {
-    let name = new urlParser.URL(link).searchParams.get('dn')
+  let name = new urlParser.URL(link).searchParams.get('dn');
 
-    if (!name) {
-        let dnF = link.substring(link.indexOf('dn='))
+  if (!name) {
+    const dnF = link.substring(link.indexOf('dn='));
 
-        name = decodeURI(dnF.substring(3, dnF.indexOf('&amp')))
-    }
+    name = decodeURI(dnF.substring(3, dnF.indexOf('&amp')));
+  }
 
-    return name
+  return name;
 }
 
 function isValidMagnetLink(link) {
-    return link.startsWith('magnet:')
+  return link.startsWith('magnet:');
 }
 
 module.exports = {
-    extractMagnet: (urls) => {
-        return new Promise((resolve, reject) => {
-            extractMagnet(urls)
-                .then(resolve)
-                .catch(reject)
-        })
-    }
-}
+  extractMagnet
+};
